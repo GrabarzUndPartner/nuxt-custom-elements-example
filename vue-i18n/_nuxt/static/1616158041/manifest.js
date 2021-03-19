52c0354ecd34084dcefe7cd190d49388ff9063a1
ThornWalli <ThornWalli@users.noreply.github.com>
2021-03-19 12:48:44 +0000

    deploy: 66fbc5aab1292c51abd5d5086e4f691ce909b176

---
+++ vue-i18n/_nuxt/static/1616158041/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Fde","\u002Fde\u002Fframes\u002Fcustom","\u002Fja\u002Fframes\u002Fcustom","\u002F","\u002Fframes\u002Fcustom","\u002Fframes\u002Fnuxt","\u002Fde\u002Fframes\u002Fnuxt","\u002Fja\u002Fframes\u002Fnuxt","\u002Fja"]})
